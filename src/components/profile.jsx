2481bc851ba2d371ab8b064625dff077a6a4b350
maxinedeandrade <deandrade@posteo.com>
2025-02-27 11:02:50 -0500

    Use github pfp for now

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -8,7 +8,7 @@ export default function Profile() {
         <div class='h-1/5 bg-[#B06DBF]'></div>
         <div class='h-1/5 rounded-bl-xs bg-[#729FE4]'></div>
       </div>
-      <img class='aspect-square w-full h-full rounded-xs' src='https://placehold.co/400' />
+      <img class='aspect-square w-full h-full rounded-xs' src='https://avatars.githubusercontent.com/u/163779298?s=400&u=f0dc1184ce22232579bd7de7f51aa142f4160f60&v=4' />
     </div>
   )
 }
